13bfd038f1a0b44178fd4af41cd76e0e54cb6414
Onyeabor Joel <earlmorningstar@gmail.com>
2025-01-06 05:14:35 +0100

    Delete test.js

--- eCommerceWebApp/backend/test.js
+++
@@ -1,5 +0,0 @@
-// import crypto from "crypto";
-
-// const key = crypto.randomBytes(32).toString("hex");
-// console.log("Add this to your .env file as CARD_ENCRYPTION_KEY:");
-// console.log(key);
